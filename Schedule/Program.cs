011c5f71f6070669021d21e659b3fc73078aad01
kfs121 <kfs121@naver.com>
2022-10-24 19:53:44 +0900

    7주 과제

---
+++ Schedule/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Schedule> mySchedule = new List<Schedule>();
+
+            mySchedule.Add(new Schedule(DayOfWeek.Monday, new DateTime(1, 1, 1, 10, 0, 0), "Meeting with my boss"));
+            mySchedule.Add(new Schedule(DayOfWeek.Tuesday, new DateTime(1, 1, 1, 13, 0, 0), "Presentation"));
+            mySchedule.Add(new Schedule(DayOfWeek.Wednesday, new DateTime(1, 1, 1, 16, 0, 0), "Wrap-up"));
+            mySchedule.Add(new Schedule(DayOfWeek.Sunday, new DateTime(1, 1, 1, 15, 0, 0), "Sunday schedule"));
+            mySchedule.Add(new Schedule(DayOfWeek.Thursday, new DateTime(1, 1, 1, 19, 0, 0), "19h schedule"));
+
+            foreach (Schedule s in mySchedule)
+            {
+                Console.WriteLine($"{s.MyDayOfWeek} {s.MyTime.ToString("t")} : {s.MyMemo}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Schedule/Schedule.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule
+{
+    enum DayOfWeek
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Count
+    }
+    class Schedule
+    {
+        public String MyMemo { get; set; }
+        private DayOfWeek mMyDayOfWeek { get; set; }
+        public DayOfWeek MyDayOfWeek
+        {
+            get
+            {
+                return mMyDayOfWeek;
+            }
+            set
+            {
+                if(value == DayOfWeek.Sunday)
+                {
+                    mMyDayOfWeek = DayOfWeek.Monday;
+                }
+                else
+                {
+                    mMyDayOfWeek = value;
+                }
+            }
+        }
+
+        private DateTime mMyTime;
+        public DateTime MyTime
+        {
+            get
+            {
+                return mMyTime;
+            }
+            set
+            {
+                if (value.Hour > 18 || value.Hour < 9)
+                {
+                    mMyTime = new DateTime(1, 1, 1, 9, 0, 0);
+                }
+                else
+                {
+                    mMyTime = value;
+                }
+            }
+        }
+        
+    
+        public Schedule(DayOfWeek myDayOfWeek, DateTime myTime, String myMemo)
+        {
+            this.MyDayOfWeek = myDayOfWeek;     //this 안써도 되지만 가시성이 더 좋아보여서..
+            this.MyTime = myTime;
+            this.MyMemo = myMemo;
+        }
+    }
+}
