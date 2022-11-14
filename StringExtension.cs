25fa1fbe4b2e5d060c194c0747e33744d4239e15
kfs121 <kfs121@naver.com>
2022-11-14 10:43:25 +0900

    Add files via upload
    10주차

---
+++ StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    static class StringExtension
+    {
+        public static string Reverse(this string input)
+        {
+            StringBuilder stb = new StringBuilder();
+            for(int i = input.Length - 1; i >= 0; i--)
+            {
+                stb.Append(input[i]);
+            }
+
+            return stb.ToString();
+        }
+    }
+}
