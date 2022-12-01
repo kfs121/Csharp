17d3c4891e1ee4c7a956a497d4f9c3c94e2deba3
kfs121 <kfs121@naver.com>
2022-12-02 08:51:22 +0900

    12주

---
+++ gacha/gacha/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gacha
+{
+    enum EType 
+    {
+    Sword, Shield, Hat, Boots
+    }
+    class Item {
+        public  string  Id { get; private set;}
+        public  EType Type { get;private  set; }
+        public string Name { get;private set;}
+        public int Level { get;private set;}
+        public int Power{ get;private set;}
+        public int Piece{ get;private set;}
+        public float Ratio { get; private set;}
+        public int Price { get;private set;}
+        public Item(string id, EType type, string name, int level, int power, int piece, float ratio, int price) {
+            Id = id;
+            Type = type;
+            Name = name;
+            Level = level;
+            Power = power;
+            Piece = piece;
+            Ratio = ratio;
+            Price = price;
+        }
+        public Item(Item item) {
+            Id = item.Id;
+            Type = item.Type;
+            Name = item.Name;
+            Level = item.Level;
+            Power = item.Power;
+            Piece = item.Piece;
+            Ratio = item.Ratio;
+            Price = item.Price;
+        }
+        public void PrintItem() {
+            Console.WriteLine($"Id: {Id}, Type: {Type}, Name: {Name}, Level: {Level}, Power: {Power}, Piece: {Piece}, Ratio: {Ratio}, Price: {Price}");
+        }
+    }
+}
