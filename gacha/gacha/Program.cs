36b9fdb3556bb5589f905b4e40541310accac930
kfs121 <kfs121@naver.com>
2022-12-02 08:57:06 +0900

    Revert "Revert "12주""
    This reverts commit 41f4434b9f96071da59bf362a91970e947dde1d8.

---
+++ gacha/gacha/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace gacha
+{
+    class Program {
+    static void Main(string [] args)
+    {
+        GameHandler gameHandler = new GameHandler();
+        string input;
+        while (true)
+        {
+            string id;
+            PrintMenu();
+            input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine();
+                    Console.WriteLine("===== SHOP : Sword List =====");
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        gameHandler.PrintItemFromShop(i);
+                    }
+                    Console.Write("Select Id: ");
+                    id = Console.ReadLine();
+                    Console.Write("Quantity: ");
+                    int quantity = int .Parse(Console.ReadLine());
+                    gameHandler.BuyItems(id, quantity);
+                    break;
+                case "2":
+                case "3":
+                    Console.WriteLine();
+                    Console.WriteLine($"===== SHOP : L{input}Sword List =====");
+                    gameHandler.PrintItemFromShop(int.Parse(input));
+                    Console.Write("Select Id: ");
+                    id = Console.ReadLine();
+                    gameHandler.BuildHigherItem(id);
+                    break;
+                case "4":
+                    Console.WriteLine();
+                    Console.WriteLine("===== My Items List =====");
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        gameHandler.PrintItemFromMyItems(i);
+                    }
+                    Console.WriteLine($"My money: {gameHandler.MyMoney}");
+                    break;
+                case "Q":
+                case "q":
+                    Console.WriteLine("Good Bye.");
+                    gameHandler.SaveFile();
+                    return;
+            }
+        }
+    }
+    static void PrintMenu()
+    {
+        Console.WriteLine();
+        Console.WriteLine("=============================");
+        Console.WriteLine("1. Buy Item");
+        Console.WriteLine("2. Build L2 Item");
+        Console.WriteLine("3. Build L3 Item");
+        Console.WriteLine("4. Show My All Items");
+        Console.WriteLine("Q. Quit");
+        Console.WriteLine("=============================");
+        Console.Write("Select: ");
+    }
+}
+}
